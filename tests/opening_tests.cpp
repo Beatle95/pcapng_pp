fedf613f78b8f2d4444bc455be60bbf3b203e090
shark <tishenko.vlad.apc@gmail.com>
2024-01-02 13:24:14 +0300

    Fix test resources locating

--- tests/opening_tests.cpp
+++ tests/opening_tests.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 #include <filesystem>
+#include "path.h"
 #include "PcapngFileReader.h"
 
 TEST(FileReader, Opening) {
-    const auto simple_correct_path {std::filesystem::u8path("./test_resources/simple_correct.pcapng")};
+    const auto simple_correct_path {std::filesystem::u8path(test_resources_path) / "simple_correct.pcapng"};
     ASSERT_TRUE(std::filesystem::exists(simple_correct_path)) << "Test file not found";
     pcapng_pp::PcapngFileReader reader {simple_correct_path};
     EXPECT_NO_THROW(reader.open());
