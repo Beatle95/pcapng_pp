8033be9eaacba0f5e153e009125189fe3470d43a
shark <tishenko.vlad.apc@gmail.com>
2024-01-05 13:43:07 +0300

    Comment fix

--- src/pcapng_block.cpp
+++ src/pcapng_block.cpp
@@ -81,7 +81,8 @@ void PcapngBlock::parse_options(Span<const char> data) {
 PcapngSectionHeader::PcapngSectionHeader(Span<const char> data)
     : PcapngBlock {PcapngBlockType::section_header}
 {
-    //                             1                   2                   3
+    // Section header block:
+    //                         1                   2                   3
     //     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
     //    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     //  0 |                   Block Type = 0x0A0D0D0A                     |
